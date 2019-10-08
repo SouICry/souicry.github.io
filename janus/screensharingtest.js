9d6e213d4d8d73560bbe832cf53219c11ef0d892
Guyue Zhou <souicry.email@gmail.com>
2019-10-07 22:15:23 -0700

    update up

--- janus/screensharingtest.js
+++ janus/screensharingtest.js
@@ -42,8 +42,8 @@
 // in the presented order. The first working server will be used for
 // the whole session.
 //
-var server = "http://35.188.197.130/janus";
-
+//var server = "http://35.188.197.130/janus";
+var server = "http://45.32.83.195:8088";
 
 var janus = null;
 var screentest = null;
