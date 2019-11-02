fdf02bb8b551a21192dd68ae682b7478277597b7
Guyue Zhou <souicry.email@gmail.com>
2019-11-01 19:13:02 -0700

    Update textroomtest.js

--- janus/textroomtest.js
+++ janus/textroomtest.js
@@ -43,7 +43,7 @@
 // the whole session.
 //
 //var server = "http://35.188.197.130/janus";
-var server = "http://45.32.83.195:8088/janus";
+var server = "http://process.watchclub.live/stream";
 
 var janus = null;
 var textroom = null;
